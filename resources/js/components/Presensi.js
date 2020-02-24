d4c042b24ee00b54a8073592739ed4e302f28edf
Jonathan Rehuel Lewerissa <jonathan16@mhs.if.its.ac.id>
2020-02-24 18:25:41 +0700

    change date

--- resources/js/components/Presensi.js
+++ resources/js/components/Presensi.js
@@ -103,7 +103,7 @@ const Presensi = (props) => {
                                     )}
                                 </form>
                             </div>
-                            <h2 className="text-xs font-hairline text-gray-500 hover:text-black text-center">{"Made with ❤ by PMK ITS - 2019"}</h2>
+                            <h2 className="text-xs font-hairline text-gray-500 hover:text-black text-center">{"Made with ❤ by PMK ITS - 2020"}</h2>
                         </div>
                     </div>
                 </div>
