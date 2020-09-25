c643b58b51fc3b8b52af517757502c8cc31c9c01
kevinanggaaa <kevin.anggahung@gmail.com>
2020-09-25 15:03:21 +0700

    bug tanggal hari fixed

--- resources/views/mahasiswa/show.blade.php
+++ resources/views/mahasiswa/show.blade.php
@@ -3,46 +3,50 @@
 @section('title', 'Mahasiswa')
 
 @section('content_header')
-    <h1>Dashboard Mahasiswa</h1>
+<h1>Dashboard Mahasiswa</h1>
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-md-12">
-            <div class="box box-info">
-                <div class="box-header with-border">
-                    <h4>Detail Mahasiswa</h4>
-                </div>
-                <div class="box-body">
-                    <table class="table" width="100%">
-                        <tbody>
+<div class="row">
+    <div class="col-md-12">
+        <div class="box box-info">
+            <div class="box-header with-border">
+                <h4>Detail Mahasiswa</h4>
+            </div>
+            <div class="box-body">
+                <table class="table" width="100%">
+                    <tbody>
                         @foreach($mahasiswa as $key => $value)
-                            <tr>
-                                <td style="font-weight: bold">{{Str::title(str_replace('_', ' ', $key))}}</td>
-                                <td>:</td>
-                                @if($key == 'tanggal_lahir')
-                                    <td>{{\Carbon\Carbon::parse($value)->toFormattedDateString()}}</td>
-                                @else
-                                    <td>{{$value}}</td>
-                                @endif
-                            </tr>
+                        <tr>
+                            <td style="font-weight: bold">{{Str::title(str_replace('_', ' ', $key))}}</td>
+                            <td>:</td>
+                            @if($key == 'tanggal_lahir')
+                            @if(isset($mahasiswa[$key]))
+                            <td>{{\Carbon\Carbon::parse($value)->toFormattedDateString()}}</td>
+                            @else
+                            <td>------</td>
+                            @endif
+                            @else
+                            <td>{{$value}}</td>
+                            @endif
+                        </tr>
                         @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
+</div>
 @endsection
 
 @section('js')
-    <script>
-        $(document).ready( function () {
-            $('#tabel_mahasiswa').DataTable({
-                responsive: true,
-            });
-        } );
-    </script>
+<script>
+    $(document).ready(function() {
+        $('#tabel_mahasiswa').DataTable({
+            responsive: true,
+        });
+    });
+</script>
 @endsection
 
 @section('css')
